fb671f1bd0d486e75c1fd20e9beb401cb45be11d
Makay131 <mahmutenesakay9@gmail.com>
2023-12-10 19:36:25 +0300

    more on navbar styiling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,12 +4,12 @@ import NavLinks from "./navbar/navbar";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center">
+    <header className="flex justify-between items-center py-[15px] 2xl:py-[18px] bg-transparent">
       <Logo />
       <NavLinks />
-      <span className="flex items-center">
-        <DynamicIcon svgName="bell" twClass="w-6 h-6 cursor-pointer" />
-        <DynamicIcon svgName="search" twClass="w-6 h-6" />
+      <span className="flex items-center gap-3.5 2xl:gap-7">
+        <DynamicIcon svgName="bell" twClass="w-6 h-6 2xl:w-[34px] 2xl:h-[34px] cursor-pointer" />
+        <DynamicIcon svgName="search" twClass="w-6 h-6 2xl:w-[34px] 2xl:h-[34px] cursor-pointer" />
       </span>
     </header>
   )
--- src/components/header/navbar/navbar.tsx
+++ src/components/header/navbar/navbar.tsx
@@ -1,12 +1,16 @@
+"use client";
+
 import headerConfig from "@/configs/header/headerConfig.json";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 export default function NavLinks() {
     const { navLinks } = headerConfig;
+    const pathname = usePathname();
     return (
-            <ul className="flex items-center">
+            <ul className="flex items-center bg-vibe-black-06 rounded-lg border-[3px] 2xl:border-4 py-2 2xl:py-2.5 pl-2 2xl:pl-2.5 pr-[30px] 2xl:pr-10 border-vibe-black-12 gap-4 2xl:gap-[30px]">
                 {navLinks.map(link => (
                     <Link key={link.key} href={link.path}>
-                        <li >{link.name}</li>
+                        <li className={`text-vibe-grey-75 text-sm 2xl:text-lg font-normal leading-[21px] 2xl:leading-[27px] hover:text-vibe-white duration-300 ${pathname === link.path ? 'py-3 2xl:py-[14px] px-5 2xl:px-6 rounded-lg bg-vibe-black-10 border border-vibe-black-10 text-vibe-white font-medium' : ''}`}>{link.name}</li>
                     </Link> 
                 ))}
             </ul>
