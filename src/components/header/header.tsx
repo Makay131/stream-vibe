d7e56692d8d733e2b7edbe2cca42b304d72af091
Makay131 <mahmutenesakay9@gmail.com>
2023-12-10 20:44:31 +0300

    navbar logo is fixed

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,11 +6,13 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center py-[15px] 2xl:py-[18px] bg-transparent">
       <Logo />
+      <>
       <NavLinks />
       <span className="flex items-center gap-3.5 2xl:gap-7">
         <DynamicIcon svgName="bell" twClass="w-6 h-6 2xl:w-[34px] 2xl:h-[34px] cursor-pointer" />
         <DynamicIcon svgName="search" twClass="w-6 h-6 2xl:w-[34px] 2xl:h-[34px] cursor-pointer" />
       </span>
+      </>
     </header>
   )
 }
--- src/components/header/logo/logo.tsx
+++ src/components/header/logo/logo.tsx
@@ -3,8 +3,8 @@ export default function Logo() {
     const { logoDesktop, desktopBreakpoint, logoLaptop, laptopBreakpoint, logoMobile } = headerConfig;
     return (
         <picture>
-            <source media={logoDesktop} srcSet={desktopBreakpoint}/>
-            <source media={logoLaptop} srcSet={laptopBreakpoint}/>
+            <source media={`(min-width: ${desktopBreakpoint})`} srcSet={logoDesktop} />
+            <source media={`(min-width: ${laptopBreakpoint})`} srcSet={logoLaptop} />
             <img src={logoMobile} alt="StreamVibe Logo" />
         </picture>
     )
