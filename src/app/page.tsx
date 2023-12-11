ffdd73c1917e58aea53b03309281309c90f3a654
Makay131 <mahmutenesakay9@gmail.com>
2023-12-11 21:21:54 +0300

    ...hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import homeConfig from "@/configs/home/homeConfig.json";
 
 export default function Home() {
+  const { heroDesktop, heroLaptop, heroMobile, heroTitle, heroDescription } = homeConfig;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
+    <main>
+wtf
     </main>
   )
 }
